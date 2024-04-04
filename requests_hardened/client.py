d800a8c8f242875776b722cd9a9392c133e49a2b
Marcin Wcisło <115464873+wcislo-saleor@users.noreply.github.com>
2024-04-04 15:49:35 +0200

    bug: copy headers instead of modyfing in-place (#18)
    Some code bases expect headers to be immutable and thus mutating them can lead to unexpected behaviors. We changed our code to make a copy of the headers.

--- requests_hardened/client.py
+++ requests_hardened/client.py
@@ -31,10 +31,13 @@ class HTTPSession(requests.Session):
     def prepare_request(self, request: Request) -> PreparedRequest:
         url = request.url
         if self._config.ip_filter_enable is True:
-            headers = request.headers or {}
-            # Cast potentially immutable header list to `dict`
-            if not isinstance(headers, dict):
-                headers = cast(dict, dict(**headers))
+            # IP filter will change headers, ensure copying because headers might be a global variable used by other requests
+            # ex. https://github.com/lepture/authlib/blob/a7d68b4c3b8a3a7fe0b62943b5228669f2f3dfec/authlib/oauth2/client.py#L205-L206
+            if request.headers:
+                headers = dict(**request.headers)
+            else:
+                headers = {}
+
             # Cast `bytes` to `str`
             if isinstance(url, bytes):
                 url = url.decode()
