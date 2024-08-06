769083abb40769bc574c40f48428d87a344b0e32
Tomasz Szymanski <lime129@gmail.com>
2024-08-06 14:15:08 +0200

    use pytest raises instead of try except

--- tests/test_ip_filter.py
+++ tests/test_ip_filter.py
@@ -1,5 +1,6 @@
 import socket
 import requests
+import pytest
 from unittest import mock
 
 from requests_hardened.ip_filter import filter_request
@@ -8,10 +9,8 @@ from requests_hardened.ip_filter import filter_request
 @mock.patch("requests_hardened.ip_filter.parse_url")
 def test_filter_request_handles_parse_url_raise_invalid_url(parse_url_mock):
     parse_url_mock.side_effect = ValueError
-    try:
+    with pytest.raises(requests.exceptions.InvalidURL):
         filter_request("improper_addr", headers={}, allow_loopback=False)
-    except requests.exceptions.InvalidURL as e:
-        assert str(e) == ""
 
 
 @mock.patch("requests_hardened.ip_filter.get_ip_address")
@@ -19,16 +18,12 @@ def test_filter_request_failed_to_resolve_domain_raise_connection_error(
     get_ip_address_mock,
 ):
     get_ip_address_mock.side_effect = socket.gaierror
-    try:
+    with pytest.raises(requests.ConnectionError, match="Failed to resolve domain"):
         filter_request("https://example.com", headers={}, allow_loopback=False)
-    except requests.ConnectionError as e:
-        assert str(e) == "Failed to resolve domain"
 
 
 @mock.patch("requests_hardened.ip_filter.get_ip_address")
 def test_filter_request_timeout_raise_timeout_error(get_ip_address_mock):
     get_ip_address_mock.side_effect = socket.timeout
-    try:
+    with pytest.raises(requests.ConnectionError, match="Failed to connect to host"):
         filter_request("https://example.com", headers={}, allow_loopback=False)
-    except requests.ConnectionError as e:
-        assert str(e) == "Failed to connect to host"
