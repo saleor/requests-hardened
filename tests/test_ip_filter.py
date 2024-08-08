535dc1750b6cd848b75feba098654c22ddd0fc9e
Tomasz Szymanski <lime129@gmail.com>
2024-08-08 12:33:10 +0200

    Tests improvements

--- tests/test_ip_filter.py
+++ tests/test_ip_filter.py
@@ -7,24 +7,25 @@ from unittest import mock
 from requests_hardened.ip_filter import filter_request
 
 
-@mock.patch("requests_hardened.ip_filter.parse_url")
-def test_filter_request_handles_parse_url_raise_invalid_url(parse_url_mock):
-    parse_url_mock.side_effect = ValueError
+def test_filter_request_handles_parse_url_raise_invalid_url():
     with pytest.raises(InvalidURL):
-        filter_request("improper_addr", headers={}, allow_loopback=False)
+        filter_request("00000001:0000C2934AB1", headers={}, allow_loopback=False)
 
 
-@mock.patch("requests_hardened.ip_filter.get_ip_address")
-def test_filter_request_failed_to_resolve_domain_raise_connection_error(
-    get_ip_address_mock,
+@mock.patch("requests_hardened.ip_filter.socket.getaddrinfo")
+def test_filter_request_raise_connection_error_on_invalid_address_family(
+    getaddrinfo_mock,
 ):
-    get_ip_address_mock.side_effect = socket.gaierror
-    with pytest.raises(requests.ConnectionError, match="Failed to resolve domain"):
+    getaddrinfo_mock.side_effect = socket.gaierror
+    with pytest.raises(
+        requests.ConnectionError,
+        match="Failed to resolve domain",
+    ):
         filter_request("https://example.com", headers={}, allow_loopback=False)
 
 
-@mock.patch("requests_hardened.ip_filter.get_ip_address")
-def test_filter_request_timeout_raise_timeout_error(get_ip_address_mock):
-    get_ip_address_mock.side_effect = socket.timeout
-    with pytest.raises(requests.ConnectionError, match="Failed to connect to host"):
+@mock.patch("requests_hardened.ip_filter.socket.getaddrinfo")
+def test_filter_request_timeout_raise_timeout_error(getaddrinfo_mock):
+    getaddrinfo_mock.side_effect = socket.timeout
+    with pytest.raises(requests.ConnectTimeout, match="Failed to connect to host"):
         filter_request("https://example.com", headers={}, allow_loopback=False)
